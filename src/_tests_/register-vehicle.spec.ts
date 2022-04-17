e1bd731fc0e20ef0e72f9d237df69069697bf1ca
Miguel Torrealba <devemtv@gmail.com>
2022-04-17 16:15:25 -0400

    refactor: add RegisterVehicle

--- src/_tests_/register-vehicle.spec.ts
+++ src/_tests_/register-vehicle.spec.ts
@@ -5,9 +5,10 @@ describe('RegisterVehicle', () => {
     const sut = new RegisterVehicle()
     const httpRequest = {
       body: {
-        // name: 'Chery',
         model: 'Tiggo 2',
-        year: 2020
+        year: 2020,
+        color: 'black'
+        // name: 'Chery'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -21,13 +22,46 @@ describe('RegisterVehicle', () => {
     const httpRequest = {
       body: {
         name: 'Chery',
+        year: 2020,
+        color: 'black'
         // model: 'Tiggo 2',
-        year: 2020
       }
     }
     const httpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Error in the: model'))
+  })
+
+  test('is the color does not exist return 400', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Chery',
+        model: 'Tiggo 2',
+        year: 2020
+        // color: 'black'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Error in the: color'))
+  })
+
+  test('its ok, return 201', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Chery',
+        model: 'Tiggo 2',
+        year: 2020,
+        color: 'black'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(201)
+    expect(httpResponse.body).toEqual('Vehicle is created')
   })
 })
--- src/controllers/register-vehicle.ts
+++ src/controllers/register-vehicle.ts
@@ -2,17 +2,13 @@ import { httpRequest, httpResponse } from '../interfaces/http-interfaces'
 
 export class RegisterVehicle {
   handle (httpRequest: httpRequest): httpResponse {
-    if (!('name' in httpRequest.body)) {
-      return {
-        statusCode: 400,
-        body: new Error('Error in the: name')
-      }
-    }
-
-    if (!('model' in httpRequest.body)) {
-      return {
-        statusCode: 400,
-        body: new Error('Error in the: model')
+    const requiredProperties = ['name', 'model', 'year', 'color']
+    for (const prop of requiredProperties) {
+      if (!(prop in httpRequest.body)) {
+        return {
+          statusCode: 400,
+          body: new Error(`Error in the: ${prop}`)
+        }
       }
     }
 
