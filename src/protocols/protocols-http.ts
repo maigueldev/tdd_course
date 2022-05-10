4ab5ecee24ba3315000011706ea21071f5eae61c
Miguel Torrealba <devemtv@gmail.com>
2022-05-10 17:38:05 -0400

    refactor: add InvalidArguments in protocols-http

--- src/protocols/protocols-http.ts
+++ src/protocols/protocols-http.ts
@@ -1,8 +1,12 @@
 // eslint-disable-next-line node/no-deprecated-api
 import { parse, UrlWithParsedQuery } from 'url'
+import { InvalidArgument } from '../errors/invalid-arg'
 
 export class UrlRequest {
   public static parseUrl (url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new InvalidArgument(url)
+    }
     return parse(url, true)
   }
 }
